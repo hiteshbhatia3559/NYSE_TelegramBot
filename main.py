a87e83ef011648fa58fe8d7860c60d3cc0ad0ce7
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-07 17:05:27 +0530

    Telegram functionality finished. Project completed, pending testing.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from multiprocessing import Process
 import nyselib
 import ast
-import time
-
+import requests
 
 bot_token = '791747943:AAFBMAXrJV_oIRQt6GmHfbUMaykyki6Sh2c'
+
+url = "https://api.telegram.org/bot" + bot_token
+
+
+def send_mess(text):
+    params = {'chat_id': '-364692428', 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
 
 symbols = ast.literal_eval(open('symbols.txt', 'r').read())
 
@@ -15,9 +23,13 @@ def one_minute():
             try:
                 rsi = nyselib.get_one_minute_rsi(symbol)
                 if rsi < 30:
-                    print("{}'s One Minute RSI is oversold, value = {}".format(symbol, rsi))
+                    response = "{}'s One Minute RSI is oversold, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
                 if rsi > 70:
-                    print("{}'s One Minute RSI is overbought, value = {}".format(symbol, rsi))
+                    response = "{}'s One Minute RSI is overbought, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
 
             except:
                 continue
@@ -29,9 +41,13 @@ def five_minute():
             try:
                 rsi = nyselib.get_five_minute_rsi(symbol)
                 if rsi < 30:
-                    print("{}'s Five Minute RSI is oversold, value = {}".format(symbol, rsi))
+                    response = "{}'s Five Minute RSI is oversold, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
                 if rsi > 70:
-                    print("{}'s Five Minute RSI is overbought, value = {}".format(symbol, rsi))
+                    response = "{}'s Five Minute RSI is overbought, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
             except:
                 continue
 
@@ -42,9 +58,13 @@ def fifteen_minute():
             try:
                 rsi = nyselib.get_fifteen_minute_rsi(symbol)
                 if rsi < 30:
-                    print("{}'s Fifteen Minute RSI is oversold, value = {}".format(symbol, rsi))
+                    response = "{}'s Fifteen Minute RSI is oversold, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
                 if rsi > 70:
-                    print("{}'s Fifteen Minute RSI is overbought, value = {}".format(symbol, rsi))
+                    response = "{}'s Fifteen Minute RSI is overbought, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
             except:
                 continue
 
@@ -55,9 +75,13 @@ def thirty_minute():
             try:
                 rsi = nyselib.get_thirty_minute_rsi(symbol)
                 if rsi < 30:
-                    print("{}'s Thirty Minute RSI is oversold, value = {}".format(symbol, rsi))
+                    response = "{}'s Thirty Minute RSI is oversold, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
                 if rsi > 70:
-                    print("{}'s Thirty Minute RSI is overbought, value = {}".format(symbol, rsi))
+                    response = "{}'s Thirty Minute RSI is overbought, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
             except:
                 continue
 
@@ -68,9 +92,13 @@ def one_day():
             try:
                 rsi = nyselib.get_one_day_rsi(symbol)
                 if rsi < 30:
+                    response = "{}'s Daily RSI is oversold, value = {}".format(symbol, rsi)
                     print("{}'s Daily RSI is oversold, value = {}".format(symbol, rsi))
+                    send_mess(response)
                 if rsi > 70:
-                    print("{}'s Daily RSI is overbought, value = {}".format(symbol, rsi))
+                    response = "{}'s Daily RSI is overbought, value = {}".format(symbol, rsi)
+                    print(response)
+                    send_mess(response)
             except:
                 continue
 
